d91c9a2caf8ba5621d4c4246e325dcd3ca9c1502
Ivan Bodnarash <bodnarash1999@gmail.com>
2023-10-26 11:27:08 -0600

    config commit2

---
+++ dropbox.config.php
@@ -0,0 +1,11 @@
+<?php
+$appKey = '3bbpgkxqnvzkrds';
+$appSecret = 'nhgmcersm4t6o1e';
+$accessToken = 'sl.Bor1aJ9bD_UfI67vx3GNtncE_1TDaiD2DFW-DZ79guGpaOrnUU7UW7R1lT1zHC3GOvA0ONKplxNc8P3_hIZwhLy1Y3EadoiYXhhd2KnGU7nEveuEFr8HH5GYcAo412-JQZ1cvPP7F13O';
+
+//
+
+// $client = new Dropbox\Client($accessToken, $appKey, $appSecret);
+
+
+?>
