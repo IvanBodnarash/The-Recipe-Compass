c3f68180e9935e446b9062aca13387396d4b9c5d
Ivan Bodnarash <bodnarash1999@gmail.com>
2023-11-03 20:16:16 -0600

    composer update commit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'da65a86de999d5c934f2d3ce843c3f5f97d35b2f',
+        'reference' => 'd2d8cf87cdaa3638cb59efd72b8793b1c3bc6e9c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'da65a86de999d5c934f2d3ce843c3f5f97d35b2f',
+            'reference' => 'd2d8cf87cdaa3638cb59efd72b8793b1c3bc6e9c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
