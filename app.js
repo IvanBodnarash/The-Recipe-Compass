98f7c327feacae354a479f2e76c7d22ff6e555d6
Ivan Bodnarash <bodnarash1999@gmail.com>
2023-11-08 22:43:05 -0700

    script adjustments commit

--- app.js
+++ app.js
@@ -3,25 +3,21 @@ const client = filestack.init('AwxQNt5QZCr9LcJtnxGBQz');
 const imageInput = document.getElementById('upload-button');
 const imageUrlInput = document.getElementById('image-url');
 
-if (imageInput) {
-    // Attaching an image upload event
-    imageInput.addEventListener('change', (e) => {
-        const file = e.target.files[0];
-        // Image uploading via Filestack
-        client.upload(file).then((response) => {
-            const imageUrl = response.url;
-            console.log("URL: " + imageUrl);
-            
-            // Save url into hidden input
-            imageUrlInput.value = imageUrl;
-            console.log("URL: " + imageUrlInput.value);
-        }).catch((error) => {
-            console.log(error);
-        });
+// Attaching an image upload event
+imageInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    // Image uploading via Filestack
+    client.upload(file).then((response) => {
+        const imageUrl = response.url;
+        console.log("URL: " + imageUrl);
+        
+        // Save url into hidden input
+        imageUrlInput.value = imageUrl;
+        console.log("URL: " + imageUrlInput.value);
+    }).catch((error) => {
+        console.log(error);
     });
-} else {
-    console.log('Error');
-}
+});
 // const client = filestack.init('AwxQNt5QZCr9LcJtnxGBQz');
 // const imageInput = document.getElementById('upload-button');
 
