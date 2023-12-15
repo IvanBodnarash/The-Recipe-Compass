59688604a1ea9f856a768a07158828414dbab367
Ivan Bodnarash <bodnarash1999@gmail.com>
2023-12-15 16:18:15 -0700

    Layout adj commit

--- register.inc.php
+++ register.inc.php
@@ -11,19 +11,19 @@
     <form class="register-form" action="index.php" method="post">
         <div class="register-form-wrapper">
             <span id="loginTxt">User Name</span>
-            <input type="text" name="userid" id="inputReg" require>
+            <input type="text" name="userid" id="inputReg" require maxlength="20">
         
             <span id="loginTxt">Password</span>
-            <input type="password" name="password" id="inputReg" require>
+            <input type="password" name="password" id="inputReg" require maxlength="30">
         
             <span id="loginTxt">Confirm Password</span>
-            <input type="password" name="password2" id="inputReg" require>
+            <input type="password" name="password2" id="inputReg" require maxlength="15">
         
             <span id="loginTxt">Full name</span>
-            <input type="text" name="fullname" id="inputReg" require>
+            <input type="text" name="fullname" id="inputReg" require maxlength="30">
         
             <span id="loginTxt">E-mail address</span>
-            <input type="text" name="email" id="inputReg" require>
+            <input type="text" name="email" id="inputReg" require maxlength="20">
         
             <input type="submit" value="REGISTER" name="" id="registerBtn" require>
 
